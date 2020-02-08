e4593d4a999d4294d1a399b68399b0c58ff651a8
Richard Quinlivan <rquinli1@binghamton.edu>
2020-02-08 17:52:00 -0500

    added question3

--- main.py
+++ main.py
@@ -4,27 +4,31 @@ import pickle
 app = Flask(__name__)
 
 @app.route("/question1.html")
+@app.route("/question2.html")
+@app.route("/question3.html")
+
 @app.route("/")
 
 def question1():
     return render_template("question1.html")
 
-@app.route("/question1", methods=["POST"])
-def getValue():
-    # import pdb; pdb.set_trace()
+@app.route("/question1", methods=["GET", "POST"])
+def getValue1():
     value = request.form["value"]
-    # result = my_model(input=valiue)
 
-    return render_template("question2")
+    return render_template("question2.html")
 
-@app.route("/question2", methods=["POST"])
-def getValue():
-    # import pdb; pdb.set_trace()
+@app.route("/question2", methods=["Get", "POST"])
+def getValue2():
     value = request.form["value"]
-    # result = my_model(input=valiue)
+
+    return render_template("question3.html")
+
+@app.route("/question3", methods=["GET", "POST"])
+def getValue3():
+    value = request.form["value"]
 
     return str(value)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
