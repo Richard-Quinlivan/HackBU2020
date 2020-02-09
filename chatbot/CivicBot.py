ca7aa0c05f678aa0ff46ce5acb5f053df3574816
NickPellegrino7 <npelleg1@binghamton.edu>
2020-02-08 20:01:55 -0500

    New Stuff

--- chatbot/CivicBot.py
+++ chatbot/CivicBot.py
@@ -10,11 +10,18 @@ class CivicBot:
         # Constants
         self.BOT_ONE = ChatBot1()
         self.BOT_TWO = ChatBot2()
-        self.FOLLOW_UP_QUESTIONS = ['Could you tell me a bit about your stance on abortions in America?', 'What is your opinion on empowering large corporations in America?', 'What do you think about the American prison system?', 'Tell about your opinions on the American education system.', 'Could you elaborate on your opinions regarding the environment?', 'What do you think about the size of our government? Should the government be less involved in the lives of its citizens?', 'Tell me more about your stance on gun ownership in America', 'What do you think of the American health care system?', 'How would you feel if the American government cut down on military spending?', 'Does the national debt in America worry you?', 'Are you proud to be an American right now?', 'Do you think America has secure borders?', 'What do you think would happen if the government lowered the amount of taxes everyone has to pay?']
+        self.DONE_STATE = 3
+        self.FOLLOW_UP_QUESTIONS = ['Could you tell me a bit about your stance on abortions in America?', 'What is your opinion on empowering large corporations in America?', 'What do you think about the American prison system?', 'Tell about your opinions on the American education system.', 'Could you tell me about your opinions regarding the environment?', 'What do you think about the size of our government? Should the government be less involved in the lives of its citizens?', 'Tell me more about your stance on gun ownership in America.', 'What do you think of the American health care system?', 'How would you feel if the American government cut down on military spending?', 'Does the national debt in America worry you?', 'Are you proud to be an American right now?', 'Do you think America has secure borders?', 'What do you think would happen if the government lowered the amount of taxes everyone has to pay?']
         # Variables
-        self.opinion = [0] * self.BOT_ONE.getSize() # STATE: 0 = NOT SPECIFIED, 1 = FOR, 2 = AGAINST
-        self.done = 0; # STATE: 0 = JUST STARTED, 1 = ON THE FIRST QUESTION, 2 = ON THE SECOND QUESTION, 3 = DONE
+        self.opinion = [0] * self.BOT_ONE.getSize() # STATE: 0 = NOT SPECIFIED, 1 = STRONG FOR, 2 = WEAK FOR, 3 = WEAK AGAINST, 4 = STRONG AGAINST
+        self.done = 0; # STATE: 0 = JUST STARTED, 1 = ON THE SECOND QUESTION, 2 = ON THE THIRD QUESTION, 3 = DONE
+        #self.done = 0; # STATE: 0 = JUST STARTED, 1 = ON THE SECOND QUESTION, 2 = DONE
         self.follow_ups = []
+        self.currentOpinionState = 0;
+        self.opinionTime = False;
+        self.notEnoughInfo = False;
+        self.knowledgeGain = 0;
+        self.alreadyTried = False
 
     def reset(self):
         self.opinion = [0] * self.BOT_ONE.getSize()
@@ -22,12 +29,42 @@ class CivicBot:
         self.follow_ups = []
 
     def getReply(self, sentence):
-        if self.follow_ups == []:
-            self.opinion += 1
-            if self.opinion >= 3:
-                return "NULL"
-            if self.opinion <= 1:
-                return "When it comes to politics, what's important to you? Feel free to speak candidly."
-            return "Could you tell me about a couple more policies that matter a lot to you?"
+        if self.opinionTime:
+            self.opinion[self.currentOpinionState] = self.BOT_TWO.classify(sentence)[0][0] + 1
+            print("New Opinion: ", self.opinion)
+            self.knowledgeGain += 1
         else:
-            nextNum = self.follow_ups.pop()
+            classify = self.BOT_ONE.classify(sentence)
+            print("Classifications Found: ", classify)
+            if classify == []:
+                return "Could you tell me a bit more information?"
+            for tag in classify:
+                self.follow_ups.append(tag)
+            self.opinionTime = True
+        while self.follow_ups != []:
+            nextNum = self.follow_ups.pop()[0]
+            print(nextNum)
+            if self.opinion[nextNum] == 0:
+                self.notEnoughInfo = False
+                self.currentOpinionState = nextNum
+                self.alreadyTried = False
+                return self.FOLLOW_UP_QUESTIONS[nextNum]
+        ## If we're here, then it is True that: self.follow_ups = []
+        if self.notEnoughInfo and not self.alreadyTried:
+            ## If we're here, then we didn't get any new information
+            self.alreadyTried = True
+            return "Could you tell me a bit more information? When it comes to politics, what else is important to you?"
+        print("Next Step")
+        self.opinionTime = False
+        self.alreadyTried = False
+        self.done += 1
+        if (self.done >= self.DONE_STATE) or ((self.done + 1 >= self.DONE_STATE) and (self.knowledgeGain >= 4)):
+            print("Conversation Over")
+            return "NULL"
+        print("Next Question")
+        self.notEnoughInfo = True
+        if self.done <= 1:
+            return "When it comes to politics, what else is important to you? Once again, feel free to speak candidly."
+        return "I'm getting some good information! Could you tell me about one or two more policies that are important to you?"
+    def getFinal():
+        return self.opinion
--- chatbot/main.py
+++ chatbot/main.py
@@ -6,7 +6,20 @@ from CivicBot import CivicBot
 
 def main():
     bot = CivicBot()
-    
+    outputSentence = ""
+
+    # First question
+    print("\nWhen it comes to politics, what's important to you? Feel free to speak candidly.")
+    inputSentence = input("\nEnter input: ")
+
+    # The rest of the conversation
+    while outputSentence != "NULL":
+        outputSentence = bot.getReply(inputSentence)
+        print("\n" + outputSentence)
+        if outputSentence != "NULL":
+            inputSentence = input("\nEnter input: ")
+
+    print("Final Opinion: ", bot.opinion)
 
 if __name__ == '__main__':
     main()
