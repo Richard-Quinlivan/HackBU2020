0bb23cf8918977e053487ecc0df5bb7a83589317
Richard Quinlivan <rquinli1@binghamton.edu>
2020-02-08 20:29:11 -0500

    added mome and face html as well as navbar

--- main.py
+++ main.py
@@ -2,25 +2,21 @@ from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-
-@app.route("/question1.html")
-@app.route("/question2.html")
-@app.route("/question3.html")
-@app.route("/question4.html")
-@app.route("/question5.html")
-@app.route("/question6.html")
-@app.route("/question7.html")
-@app.route("/question8.html")
-@app.route("/question9.html")
-@app.route("/question10.html")
-@app.route("/question11.html")
-@app.route("/question12.html")
-@app.route("/question13.html")
-@app.route("/question14.html")
-@app.route("/question15.html")
+@app.route("/Home.html")
 
 @app.route("/")
 
+
+
+def Home():
+    return render_template("Home.html")
+
+@app.route("/face.html")
+@app.route("/face", methods=["GET", "POST"])
+def face():
+    return render_template("face.html")
+
+@app.route("/question1.html")
 def question1():
     return render_template("question1.html")
 
@@ -30,6 +26,7 @@ def getValue1():
 
     return render_template("question2.html", prevInput1 = input)
 
+@app.route("/question2.html")
 @app.route("/question2", methods=["Get", "POST"])
 def getValue2():
     prevIn1 = request.form["pInput1"]
@@ -37,6 +34,7 @@ def getValue2():
 
     return render_template("question3.html",prevInput1 = prevIn1, prevInput2 = input)
 
+@app.route("/question3.html")
 @app.route("/question3", methods=["GET", "POST"])
 def getValue3():
     prevIn1 = request.form["pInput1"]
@@ -45,6 +43,7 @@ def getValue3():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = input)
 
+@app.route("/question4.html")
 @app.route("/question4", methods=["GET", "POST"])
 def getValue4():
     prevIn1 = request.form["pInput1"]
@@ -54,6 +53,7 @@ def getValue4():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = input)
 
+@app.route("/question5.html")
 @app.route("/question5", methods=["GET", "POST"])
 def getValue5():
     prevIn1 = request.form["pInput1"]
@@ -65,6 +65,7 @@ def getValue5():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = input)
 
+@app.route("/question6.html")
 @app.route("/question6", methods=["GET", "POST"])
 def getValue6():
     prevIn1 = request.form["pInput1"]
@@ -78,6 +79,7 @@ def getValue6():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = input)
 
+@app.route("/question7.html")
 @app.route("/question7", methods=["GET", "POST"])
 def getValue7():
     prevIn1 = request.form["pInput1"]
@@ -91,6 +93,7 @@ def getValue7():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = input)
 
+@app.route("/question8.html")
 @app.route("/question8", methods=["GET", "POST"])
 def getValue8():
     prevIn1 = request.form["pInput1"]
@@ -105,6 +108,7 @@ def getValue8():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = input)
 
+@app.route("/question9.html")
 @app.route("/question9", methods=["GET", "POST"])
 def getValue9():
     prevIn1 = request.form["pInput1"]
@@ -120,6 +124,7 @@ def getValue9():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = previn8, prevInput9 = input)
 
+@app.route("/question10.html")
 @app.route("/question10", methods=["GET", "POST"])
 def getValue10():
     prevIn1 = request.form["pInput1"]
@@ -136,6 +141,7 @@ def getValue10():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = prevIn8, prevInput9 = prevIn9, prevInput10 = input)
 
+@app.route("/question11.html")
 @app.route("/question11", methods=["GET", "POST"])
 def getValue11():
     prevIn1 = request.form["pInput1"]
@@ -153,6 +159,7 @@ def getValue11():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = prevIn8, prevInput9 = prevIn9, prevInput10 = prevIn10, prevInput11 = input)
 
+@app.route("/question12.html")
 @app.route("/question12", methods=["GET", "POST"])
 def getValue12():
     prevIn1 = request.form["pInput1"]
@@ -171,6 +178,7 @@ def getValue12():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = prevIn8, prevInput9 = prevIn9, prevInput10 = prevIn10, prevInput11 = prevIn11, prevInput12 = input)
 
+@app.route("/question13.html")
 @app.route("/question13", methods=["GET", "POST"])
 def getValue13():
     prevIn1 = request.form["pInput1"]
@@ -190,6 +198,7 @@ def getValue13():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = prevIn8, prevInput9 = prevIn9, prevInput10 = prevIn10, prevInput11 = prevIn11, prevInput12 = prevIn12, prevInput13 = input)
 
+@app.route("/question14.html")
 @app.route("/question14", methods=["GET", "POST"])
 def getValue14():
     prevIn1 = request.form["pInput1"]
@@ -210,6 +219,7 @@ def getValue14():
 
     return render_template("question4.html",prevInput1 = prevIn1, prevInput2 = prevIn2, prevInput3 = prevIn3, prevInput4 = prevIn4, prevInput5 = prevIn5, prevInput6 = prevIn6, prevInput7 = prevIn7, prevInput8 = prevIn8, prevInput9 = prevIn9, prevInput10 = prevIn10, prevInput11 = prevIn11, prevInput12 = prevIn12, prevInput13 = prevIn13, prevInput14 = input)
 
+@app.route("/question15.html")
 @app.route("/question15", methods=["GET", "POST"])
 def getValue15():
     prevIn1 = request.form["pInput1"]
