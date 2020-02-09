238be4e5b5661ca24c0c4aa61614f1582413ede2
NickPellegrino7 <npelleg1@binghamton.edu>
2020-02-09 08:55:08 -0500

    Candidate Calcs

--- chatbot/candidateData/CandidateCalcs.py
+++ chatbot/candidateData/CandidateCalcs.py
@@ -1,29 +1,34 @@
+## Brendan Klayman & Nick Pellegrino
+## Opinion Similarity Calculator
+## Run this file to test the calculator
+
 import random
 
-#candidates = array of arrays for each candidates' views
-#user = array of user's views
-#returns index of closest candidate
-
+# candidates = array of arrays for each candidates' views
+# user = array of user's views
+# returns index of closest candidate
 class CandCalc:
     def findClosest(self, candidates, user):
-        largestDifference = 42
-        closestCandidates = []
+        candidatesDistances = []
         for i in range(0, len(candidates)):
             difference = 0
             for j in range(0, len(candidates[i])):
                 if(candidates[i][j] != 0):
                     difference += abs(candidates[i][j] - user[j])
-            if(difference == largestDifference):
-                closestCandidates.append(i)
-            else:
-                closestCandidates = [i]
-        if(len(closestCandidates) > 1):
-            winner = random.randInt(0, len(closestCandidates))
-            closestCandidates = [closestCandidates[winner]]
-        return closestCandidates[0]
+            print(candidates[i], " ", difference)
+            candidatesDistances.append(difference)
+        max = 0
+        index = 0
+        for i in range(len(candidatesDistances)):
+            if candidatesDistances[i] > max:
+                max = candidatesDistances[i]
+                index = i
+        bestCandidates = [i for i in range(len(candidatesDistances)) if candidatesDistances[i] >= max]
+        return random.choice(bestCandidates)
 
 def main():
     print(CandCalc().findClosest([[2,3,4,3,3,2,4,3,1,4,3,3,4], [1,3,4,2,4,1,3,3,1,4,3,3,4], [1,4,4,4,4,1,3,4,1,4,4,1,4], [1,3,4,3,3,1,4,4,4,4,3,1,4], [2,3,3,3,3,2,4,3,2,3,3,2,3], [4,1,1,1,1,4,1,1,4,1,1,4,2], [1,4,4,4,4,1,4,4,1,4,4,1,1], [2,3,4,3,1,2,3,3,4,3,3,3,3], [4,2,2,2,3,3,2,1,3,2,2,4,2], [1,3,4,2,2,1,4,3,1,4,4,1,4], [2,3,4,4,3,1,4,3,2,4,4,1,4], [4,2,2,2,4,4,2,1,3,1,2,4,2], [1,4,4,2,2,1,4,2,4,4,4,2,4], [2,3,3,2,3,2,4,3,2,3,3,2,4], [1,4,4,2,3,2,3,3,1,4,3,2,3], [3,3,4,3,4,1,3,3,2,4,3,1,4], [1,3,4,4,3,2,4,3,1,3,4,3]], [3, 0, 0, 0, 4, 0, 2, 0, 0, 0, 0, 0, 0]))
+    print(CandCalc().findClosest([[2,3,4,3,3,2,4,3,1,4,3,3,4], [1,3,4,2,4,1,3,3,1,4,3,3,4], [1,4,4,4,4,1,3,4,1,4,4,1,4], [1,3,4,3,3,1,4,4,4,4,3,1,4], [2,3,3,3,3,2,4,3,2,3,3,2,3], [4,1,1,1,1,4,1,1,4,1,1,4,2], [1,4,4,4,4,1,4,4,1,4,4,1,1], [2,3,4,3,1,2,3,3,4,3,3,3,3], [4,2,2,2,3,3,2,1,3,2,2,4,2], [1,3,4,2,2,1,4,3,1,4,4,1,4], [2,3,4,4,3,1,4,3,2,4,4,1,4], [4,2,2,2,4,4,2,1,3,1,2,4,2], [1,4,4,2,2,1,4,2,4,4,4,2,4], [2,3,3,2,3,2,4,3,2,3,3,2,4], [1,4,4,2,3,2,3,3,1,4,3,2,3], [3,3,4,3,4,1,3,3,2,4,3,1,4], [1,3,4,4,3,2,4,3,1,3,4,3]], [1,0,4,2,0,0,3,0,0,0,0,0,0])) ## Correct answer: 2
 
 if __name__ == '__main__':
     main()
